7cfc7c161ab216d339f75b125a163eb8e48da415
Helen-sew <53653038+Helen-sew@users.noreply.github.com>
2020-08-29 11:45:14 +0800

    prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const path = require('path'); //the path module provides a way of working with directories and file patchs.
 
 //connect Database
 connectDB();
@@ -12,8 +13,14 @@ app.use(express.json({ extended: false })); //allows us to get the data from req
 //routes
 require('./routes')(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+//Serve static assests in production  (for deployment to Heroku)
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  //serve or send index.html in client as static file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); // path.resolve() is to resolve path into an absolute path.
+  });
+}
 
 app.listen(PORT, () => console.log(`Server run on ${PORT}`));
